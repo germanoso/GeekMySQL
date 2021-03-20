a3768f4bb9ebf36b356735a310c296b47df3fe07
germanoso <bearsik@gmail.com>
2021-03-20 10:58:13 +0100

    Update Lesson10.sql
    Без вашего разбора ДЗ не получалось выполнить 2 задание.

--- Les10/Lesson10.sql
+++ Les10/Lesson10.sql
@@ -20,3 +20,24 @@ CREATE INDEX profiles_city_country_idx ON profiles(city, country);
 -- общее количество пользователей в группе
 -- всего пользователей в системе
 -- отношение в процентах (общее количество пользователей в группе / всего пользователей в системе) * 100
+
+
+SELECT DISTINCT 
+  communities.name AS group_name,
+  COUNT(communities_users.user_id) OVER() 
+    / (SELECT COUNT(*) FROM communities) AS avg_users_in_groups,
+  FIRST_VALUE(CONCAT_WS(" ", users.first_name, users.last_name)) OVER w_community_birthday_desc AS youngest,
+  FIRST_VALUE(CONCAT_WS(" ", users.first_name, users.last_name)) OVER w_community_birthday_asc AS oldest,
+  COUNT(communities_users.user_id) OVER w_community AS users_in_group,
+  (SELECT COUNT(*) FROM users) AS users_total,
+  COUNT(communities_users.user_id) OVER w_community / (SELECT COUNT(*) FROM users) *100 AS '%%'
+    FROM communities
+      LEFT JOIN communities_users 
+        ON communities_users.community_id = communities.id
+      LEFT JOIN users 
+        ON communities_users.user_id = users.id
+      LEFT JOIN profiles 
+        ON profiles.user_id = users.id
+      WINDOW w_community AS (PARTITION BY communities.id),
+             w_community_birthday_desc AS (PARTITION BY communities.id ORDER BY profiles.birthday DESC),
+             w_community_birthday_asc AS (PARTITION BY communities.id ORDER BY profiles.birthday); 
